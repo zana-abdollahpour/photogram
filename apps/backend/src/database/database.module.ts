0c5e582ee624295cd8d4674912b667f3e3bbd37c
zana-abdollahpour <zana.ap4@gmail.com>
2026-05-30 21:10:34 +0330

    feat(backend): add database module basics

---
+++ apps/backend/src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+import { DATABASE_CONNECTION } from 'src/database/database.connection';
+
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: DATABASE_CONNECTION,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+        });
+
+        return drizzle(pool);
+      },
+    },
+  ],
+})
+export class DatabaseModule {}
