344b8ee974191dcf6ff596aa07e9e6bc54ef356d
zana-abdollahpour <zana.ap4@gmail.com>
2026-06-01 00:17:54 +0330

    feat(web): add home page basic structure

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,18 @@
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="bg-background min-h-dvh">
+      <div className="mx-auto max-w-6xl px-4 py-8">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          <div className="space-y-6 lg:col-span-2">
+            {/* TODO: stories */}
+            {/* TODO: feed */}
+          </div>
+
+          <div className="lg:sticky lg:top-8 lg:h-fit">
+            {/* TODO: sidebar */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
