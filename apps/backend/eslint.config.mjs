71607c78f66ae96c37a991f4e0efa77351912c60
Zana Abdollahpour <zana.ap4@gmail.com>
2026-06-09 12:21:23 +0330

    chore(backend): disable some rules in eslint

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -29,7 +29,10 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
